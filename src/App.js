52bf525c86b40170e1672ed44c8c2c62e0b5fede
e-shaan <eshaan.gupta101@gmail.com>
2022-05-10 19:43:30 +0530

    replaced process.env with secrets

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ import CustomLoginPage from './CustomLoginPage';
 import EventMonitor from './EventMonitor';
 
 let firebaseConfig ={
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: secrets.REACT_APP_FIREBASE_API_KEY,
+  authDomain: secrets.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: secrets.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: secrets.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: secrets.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: secrets.REACT_APP_FIREBASE_APP_ID,
+  measurementId: secrets.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
