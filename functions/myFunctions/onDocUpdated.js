0f5ac1185a414b1a3c517ceb0ec8abb44cc1441e
e-shaan <eshaan.gupta101@gmail.com>
2022-07-09 14:53:35 +0530

    google calendar API insert-fetch working

--- functions/myFunctions/onDocUpdated.js
+++ functions/myFunctions/onDocUpdated.js
@@ -2,23 +2,6 @@ const admin = require("firebase-admin");
 const db = admin.firestore();
 
 //const addEventToCalendar = require("./addEventToCalendar");
-const scheduleBatch = async (values) => {
-  const writeResult = await admin
-    .firestore()
-    .collection("events")
-    .add({
-      event_id: values.batch_id + "_4",
-      //duration: values.duration,
-      object: {
-        title: values.teacher_name + "'s batch",
-        start: values.start_date,
-        end: values.planned_end_date,
-      },
-    });
-  console.log("Hi From Test Function!");
-  console.log("values:", values);
-  //console.log("writ", values);
-};
 
 module.exports.onDocUpdated = async (snap, context) => {
   const values = snap.after.data();
@@ -30,7 +13,82 @@ module.exports.onDocUpdated = async (snap, context) => {
   if (collectionName == "batches") {
     if (values.isScheduled == true) {
       console.log("Batch Scheduled");
-      scheduleBatch(values);
+
+      const { google } = require("googleapis");
+      const calendar = google.calendar("v3");
+      //const functions = require('firebase-functions');
+      const googleCredentials = require("./credentials.json");
+      const OAuth2 = google.auth.OAuth2;
+
+      const ERROR_RESPONSE = {
+        status: "500",
+        message: "There was an error adding an event to your Google calendar",
+      };
+      const TIME_ZONE = "IST";
+
+      // const event = {
+      //   eventName: values.batch_id,//"Firebase Event",
+      //   description: values.teacher_name +  values.course_name + values.category,
+      //   startTime: values.start_date+"T"+values.schedule[0].time+":00",//"2022-06-30T10:00:00",
+      //   endTime: "2022-06-30T13:00:00",//values.start_date+"T"+values.schedule[0].time,
+      // };
+      
+      const event = {
+        eventName: "Firebase Event",
+        description: "TESssssT",
+        location: '800 Howard St., San Francisco, CA 94103',
+        startTime: "2022-07-20T10:00:00",
+        endTime: "2022-07-20T13:00:00",//values.start_date+"T"+values.schedule[0].time,
+        
+        
+      };
+
+      console.log("Event Data: ", event);
+      const oAuth2Client = new OAuth2(
+        googleCredentials.web.client_id,
+        googleCredentials.web.client_secret,
+        googleCredentials.web.redirect_uris[0]
+      );
+
+      oAuth2Client.setCredentials({
+        refresh_token: googleCredentials.refresh_token,
+      });
+      calendar.events.insert(
+        {
+          auth: oAuth2Client,
+          calendarId: "primary",
+          resource: {
+            summary: event.eventName,
+            attendees: [
+              {email: 'lpage@example.com'},
+              {email: 'sbrin@example.com'},
+            ],
+            description: event.description,
+            start: {
+              dateTime: event.startTime,
+              timeZone: TIME_ZONE,
+            },
+            end: {
+              dateTime: event.endTime,
+              timeZone: TIME_ZONE,
+            },
+            extendedProperties: {
+              private: { key1: "value1" },
+              shared: { key2: "value2" }
+            },
+          },
+
+        },
+        (err, res) => {
+          if (err) {
+            console.log("Rejecting because of error");
+            //reject(err);
+            console.log(err);
+          }
+          console.log("Request successful");
+          //resolve(res.data);
+        }
+      );
     } else {
       console.log("Batch Unscheduled");
     }
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,108 +2,154 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Title } from "react-admin";
-import FullCalendar, {
-  computeSegEndResizable,
-  formatDate,
-} from "@fullcalendar/react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-
 import listPlugin from "@fullcalendar/list";
 // import nlLocale from '@fullcalendar/core/locales/nl'
-import firebaseConfig from "./firebaseConfig";
-import { useEffect } from "react";
-import firebase from "firebase/compat/app";
-import { it } from "date-fns/locale";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
 
-const fetchBatches = async () => {
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  const response = db.collection("batches");
-  const data = await response.get();
-  const data_array = data.docs.map((doc) => ({
-    id: doc.id,
-    data: doc.data(),
-  }));
-  console.log(data_array);
-  return data_array;
-};
-
-export default () => {
-  useEffect(() => {
-    //const start_time = fetchBatches();
-    //console.log(start_time);
-  }, []);
-
-  return (
-    <Card>
-      <Title title="Welcome to the administration" />
-      <CardContent>
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          // events={[
-          //   {
-          //     title: "event2",
-          //     start: start_time,
-          //     end: "2022-07-04",
-          //   },
-          events={async function (info, successCallback, failureCallback) {
-            const firebaseApp = firebase.initializeApp(firebaseConfig);
-            const db = firebase.firestore();
-            const response = db.collection("events");
-            const data = await response.get();
-            const data_array = data.docs.map((doc) => ({
-              id: doc.id,
-              data: doc.data(),
-            }));
-            console.log(data_array);
-            if (data_array == undefined) {
-              failureCallback("Undefined");
-            } else {
-              successCallback(
-                data_array
-                  .map(function (eventEl) {
-                    console.log(eventEl);
-                    return eventEl.data.object;
-                  })
-              );
+export default () => (
+  <Card>
+    <Title title="Welcome to the administration" />
+    <CardContent>
+      <FullCalendar
+        // locale={nlLocale}
+        plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
+        googleCalendarApiKey="AIzaSyA9LehPKRjFBzQ-AKeQS9wqoDoTbliErG8"
+        events=
+        {
+            {
+                googleCalendarId: "eshaan.gupta101@gmail.com",
             }
-          }}
-        />
+        }
+          headerToolbar={{
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,listWeek",
+        }}
+        initialView="dayGridMonth"
+        editable={false}
+        selectable={false}
+        displayEventTime={true}
+        eventTimeFormat={{
+          hour: "2-digit",
+          minute: "2-digit",
+          meridiem: false,
+          hour12: false,
+        }}
+        nextDayThreshold="23:59:00"
+        eventColor="#971849"
+        eventTextColor="#ddd"
+        firstDay={1}
+      />
+    </CardContent>
+  </Card>
+);
 
-        {/* <FullCalendar
-      
-      // locale={nlLocale}
-      plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
-      googleCalendarApiKey="AIzaSyA9LehPKRjFBzQ-AKeQS9wqoDoTbliErG8"
-      events=
-      {
-          {
-              googleCalendarId: "eshaan.gupta101@gmail.com",
 
-          }
-      }
 
-        headerToolbar={{
-        left: "prev,next",
-        center: "title",
-        right: "dayGridMonth,listWeek",
-      }}
-      initialView="dayGridMonth"
-      editable={false}
-      selectable={false}
-      displayEventTime={true}
-      eventTimeFormat={{
-        hour: "2-digit",
-        minute: "2-digit",
-        meridiem: false,
-        hour12: false,
-      }}
-      nextDayThreshold="23:59:00"
-      eventColor="#971849"
-      eventTextColor="#ddd"
-      firstDay={1}
-    /> */}
-      </CardContent>
-    </Card>
-  );
-};
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import { Title } from "react-admin";
+// import FullCalendar, {
+//   computeSegEndResizable,
+//   formatDate,
+// } from "@fullcalendar/react";
+// import dayGridPlugin from "@fullcalendar/daygrid";
+
+// import listPlugin from "@fullcalendar/list";
+// // import nlLocale from '@fullcalendar/core/locales/nl'
+// import firebaseConfig from "./firebaseConfig";
+// import { useEffect } from "react";
+// import firebase from "firebase/compat/app";
+// import { it } from "date-fns/locale";
+
+// const fetchBatches = async () => {
+//   const firebaseApp = firebase.initializeApp(firebaseConfig);
+//   const db = firebase.firestore();
+//   const response = db.collection("batches");
+//   const data = await response.get();
+//   const data_array = data.docs.map((doc) => ({
+//     id: doc.id,
+//     data: doc.data(),
+//   }));
+//   console.log(data_array);
+//   return data_array;
+// };
+
+// export default () => {
+//   useEffect(() => {
+//     //const start_time = fetchBatches();
+//     //console.log(start_time);
+//   }, []);
+
+//   return (
+//     <Card>
+//       <Title title="Welcome to the administration" />
+//       <CardContent>
+//         {
+//           /* <FullCalendar
+//           plugins={[dayGridPlugin]}
+//           // events={[ */
+//           //   {
+//           //     title: "event2",
+//           //     start: start_time,
+//           //     end: "2022-07-04",
+//           //   },
+//           //   events={async function (info, successCallback, failureCallback) {
+//           //     const firebaseApp = firebase.initializeApp(firebaseConfig);
+//           //     const db = firebase.firestore();
+//           //     const response = db.collection("events");
+//           //     const data = await response.get();
+//           //     const data_array = data.docs.map((doc) => ({
+//           //       id: doc.id,
+//           //       data: doc.data(),
+//           //     }));
+//           //     console.log(data_array);
+//           //     if (data_array == undefined) {
+//           //       failureCallback("Undefined");
+//           //     } else {
+//           //       successCallback(
+//           //         data_array
+//           //           .map(function (eventEl) {
+//           //             console.log(eventEl);
+//           //             return eventEl.data.object;
+//           //           })
+//           //       );
+//           //     }
+//           //   }}
+//           // />
+//         }
+//         <FullCalendar
+//           // locale={nlLocale}
+//           plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
+//           googleCalendarApiKey="AIzaSyA9LehPKRjFBzQ-AKeQS9wqoDoTbliErG8"
+//           events={{
+//             googleCalendarId: "eshaan.gupta101@gmail.com",
+//           }}
+//           headerToolbar={{
+//             left: "prev,next",
+//             center: "title",
+//             right: "dayGridMonth,listWeek",
+//           }}
+//           initialView="dayGridMonth"
+//           editable={false}
+//           selectable={false}
+//           displayEventTime={true}
+//           eventTimeFormat={{
+//             hour: "2-digit",
+//             minute: "2-digit",
+//             meridiem: false,
+//             hour12: false,
+//           }}
+//           nextDayThreshold="23:59:00"
+//           eventColor="#971849"
+//           eventTextColor="#ddd"
+//           firstDay={1}
+//         />{" "}
+
+//       </CardContent>
+//     </Card>
+//   );
+// };
