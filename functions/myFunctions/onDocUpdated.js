a0f55bca5d50a552c5182e4fde2c455e055a2b12
e-shaan <eshaan.gupta101@gmail.com>
2022-07-11 12:00:59 +0530

    multiple event creation working

--- functions/myFunctions/onDocUpdated.js
+++ functions/myFunctions/onDocUpdated.js
@@ -2,51 +2,23 @@ const { computeSegEndResizable } = require("@fullcalendar/react");
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
-const addEvent = (batch) => {
-  console.log("BATCH DATA" , batch);
-  const rosterArray = batch.schedule[0];
-  console.log(rosterArray);
-  
+// batch
 
+// call it 16 times
+
+// insert 1 event
+
+const insertEvent = (event) => {
   const { google } = require("googleapis");
   const calendar = google.calendar("v3");
   const googleCredentials = require("./credentials.json");
   const OAuth2 = google.auth.OAuth2;
-
   const ERROR_RESPONSE = {
     status: "500",
     message: "There was an error adding an event to your Google calendar",
   };
   const TIME_ZONE = "IST";
 
-  //  const event1 = {
-  //     eventName: values.batch_id,//"Firebase Event",
-  //     description: values.teacher_name +  values.course_name + values.category,
-  //     startTime: values.start_date+"T"+values.schedule[0].time+":00",//"2022-06-30T10:00:00",
-  //     endTime: "2022-06-30T13:00:00",//values.start_date+"T"+values.schedule[0].time,
-  //   };
-
-
-  
-  const [year, month, day] = batch.start_date.split('-');
-  const [hours, minutes] = rosterArray.time.split(':');
-  const seconds = "00";
-  let date = new Date(+year, + month - 1, +day, +hours, +minutes, +seconds);
-  const startTime= date.toISOString();
-  date = new Date(+year, + month - 1, +day, +hours, +minutes + rosterArray.duration, +seconds);
-  const endTime = date.toISOString();
-
-  
-  const event = {
-    eventName: "Firebase Event",
-    description: "TESssssT",
-    location: "800 Howard St., San Francisco, CA 94103",
-    startTime: startTime , //rosterArray.start_date + "T" + rosterArray.time  + ":00",
-    endTime: endTime, //"2022-07-20T13:00:00", //values.start_date+"T"+values.schedule[0].time,
-    //endTime: rosterArray.start_date + "T" + rosterArray.time  + ":00",
-  };
-
-  console.log("Event Data: ", event);
   const oAuth2Client = new OAuth2(
     googleCredentials.web.client_id,
     googleCredentials.web.client_secret,
@@ -56,16 +28,16 @@ const addEvent = (batch) => {
   oAuth2Client.setCredentials({
     refresh_token: googleCredentials.refresh_token,
   });
+
   calendar.events.insert(
     {
       auth: oAuth2Client,
       calendarId: "primary",
       resource: {
         summary: event.eventName,
-        location: "800 Howard St., San Francisco, CA 94103",
         attendees: [
-          { email: "lpage@example.com" },
-          { email: "sbrin@example.com" },
+          { email: "test@example.com" },
+          { email: "test@example.com" },
         ],
         description: event.description,
         start: {
@@ -81,13 +53,80 @@ const addEvent = (batch) => {
     (err, res) => {
       if (err) {
         console.log("Rejecting because of error");
-        //reject(err);
         console.log(err);
+      } else {
+        console.log("Request successful");
       }
-      console.log("Request successful");
-      //resolve(res.data);
     }
   );
+};
+
+const makeEvent = (batch) => {
+  const event = {
+    eventName: batch.batch_id,
+    description: batch.teacher_name + "'s batch.",
+    startTime: getStartTime(batch),
+    endTime: getEndTime(batch),
+  };
+  return event;
+};
+
+const getStartTime = (batch) => {
+  console.log("BATCH 75" , batch);
+  console.log("BATCH START DATE" , batch.start_date);
+  const [year, month, day] = batch.start_date.split("-");
+  const [hours, minutes] = batch.schedule[0].time.split(":");
+  const seconds = "00";
+  let date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
+  const startTime = date.toISOString();
+  return startTime;
+};
+const getEndTime = (batch) => {
+  const [year, month, day] = batch.start_date.split("-");
+  const [hours, minutes] = batch.schedule[0].time.split(":");
+  const seconds = "00";
+  let date = new Date(
+    +year,
+    +month - 1,
+    +day,
+    +hours,
+    +minutes + batch.schedule[0].duration,
+    +seconds
+  );
+  const endTime = date.toISOString();
+  return endTime;
+};
+
+const scheduleBatch = (batch) => {
+  batch = {
+    teacher_name: null,
+    planned_end_date: null,
+    updatedby: "nWuleMgKPERrsPf10aJOq2CXgSQ2",
+    course_name: null,
+    level: "1",
+    isScheduled: true,
+    actual_end_date: null,
+    schedule: [
+      { duration: 60, time: "01:43", day: "Thursday" },
+      { duration: 60, time: "11:49", day: "Tuesday" },
+    ],
+    batch_students: null,
+    sessionCount: "4",
+    createdby: "nWuleMgKPERrsPf10aJOq2CXgSQ2",
+    category: null,
+    status: null,
+    start_date: "2022-07-12",
+    batch_id: "S100",
+  };
+  let event;
+  let ctr = batch.sessionCount;
+  while (ctr != 0) {
+    event = makeEvent(batch);
+    insertEvent(event);
+    ctr--;
+  }
+
+  console.log("Event Data: ", event);
 };
 
 module.exports.onDocUpdated = async (snap, context) => {
@@ -100,7 +139,7 @@ module.exports.onDocUpdated = async (snap, context) => {
   if (collectionName == "batches") {
     if (values.isScheduled == true) {
       console.log("Batch Scheduled");
-      addEvent(values);
+      scheduleBatch(values);
     } else {
       console.log("Batch Unscheduled");
     }
